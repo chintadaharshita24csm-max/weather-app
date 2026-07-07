727a349e0e0f0d7d62628f5569704c8ec0bd4c84
chintadaharshita24csm-max <chintadaharshita.24.csm@anits.edu.in>
2026-07-07 09:14:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.querySelector("button").addEventListener("click",function(){
+  const city=document.getElementById("city").value
+document.getElementById("temperature").textcontent="Temperatur:30&deg;c";
+document.getElementById("condition").textcontent="condition:Sunny 🌞 in"+city;
+});
